8d78548454ae5050e4248db4be0e147a5e3ccdb3
rafael1996-git <quintorafael12@gmail.com>
2022-11-09 22:15:11 -0600

    segundo commit

--- src/main/java/com/ine/cartografia/controlador/Controlador.java
+++ src/main/java/com/ine/cartografia/controlador/Controlador.java
@@ -7,16 +7,15 @@ import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.ine.cartografia.dao.IServiceDao;
 import com.ine.cartografia.entity.Usuario;
-
-@Controller
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET})
+@RestController()
 public class Controlador {
 	private final Logger logger = LoggerFactory.getLogger(Controlador.class);
 
@@ -24,15 +23,11 @@ public class Controlador {
 	private IServiceDao Consulta;
 	  
 	   @GetMapping("/demo")
-	   public ModelAndView passParametersWithModelAndView(HttpServletRequest request) throws  Exception{
+	   public List<Usuario> passParametersWithModelAndView(HttpServletRequest request) throws  Exception{
 		   List<Usuario> lista = Consulta.GetAll();
 		   logger.debug("consulta "+lista);
-		   request.setAttribute("lista", lista);
-		    ModelAndView modelAndView = new ModelAndView("index");
-
-		    return modelAndView;
+		    return  lista;
 		}
-	   @ResponseBody
 	   @GetMapping("/")
 		public List<Usuario> index(HttpServletRequest request) throws  Exception{
 		   List<Usuario> lista = Consulta.GetAll();
